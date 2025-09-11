19c62f09d50fa8a952e5e0d5845ce7428d487e9e
olamilekan@root <olorunnisholaolamilekan@gmail.com>
2025-09-11 08:05:43 +0100

    feat: nested arrays recursive add

--- specs/recursion/nested-arrays.test.js
+++ specs/recursion/nested-arrays.test.js
@@ -11,11 +11,21 @@
 
 function nestedAdd(array) {
   // write code here
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'number') {
+      sum += array[i]
+    }
+    if (Array.isArray(array[i])) {
+      sum += nestedAdd(array[i])
+    }
+  }
+  return sum
 }
 
-test.skip("nested arrays addition", () => {
-  expect(nestedAdd([1, 2, 3])).toEqual(6);
-  expect(nestedAdd([1, [2], 3])).toEqual(6);
-  expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5);
-  expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94);
-});
+test('nested arrays addition', () => {
+  expect(nestedAdd([1, 2, 3])).toEqual(6)
+  expect(nestedAdd([1, [2], 3])).toEqual(6)
+  expect(nestedAdd([[[[[[[[[5]]]]]]]]])).toEqual(5)
+  expect(nestedAdd([10, [12, 14, [1], [16, [20]]], 10, 11])).toEqual(94)
+})
